27273963e9fac7c0922291da63cda0a65cf4c272
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-10-05 18:05:55 -0400

    adding data

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Washington DC - Open Data
 
 A limited collection of data from http://opendata.dc.gov/,
- copied here for reference/posterity.
+ copied here for instructional reference.
 
 ## Instructional Datasets
 
 Reference the [data directory](/data).
 
-dataset_name | file_name | downloaded_on | source_url
---- | --- | --- | ---
-Washington DC Parks | `dc_parks.csv` | 2015-10-05 | http://opendata.dc.gov/datasets/287eaa2ecbff4d699762bbc6795ffdca_9
-Washington DC Purchase Orders | `dc_purchase_orders.csv` | 2015-10-05 | http://opendata.dc.gov/datasets/f61f962c2ce84f2caa1919d425c8061d_0
+dataset_name | file_name | downloaded_on | source_url | original_file_name
+--- | --- | --- | --- | ---
+Washington DC Parks | `parks_2015.csv` | 2015-10-05 | http://opendata.dc.gov/datasets/287eaa2ecbff4d699762bbc6795ffdca_9 | `DC_Parks.csv`
+Washington DC Purchase Orders | `purchase_orders_2015.csv` | 2015-10-05 | http://opendata.dc.gov/datasets/f61f962c2ce84f2caa1919d425c8061d_0| `pass_FY2015_Feb.csv`
---
+++ support/scripts/import_purchase_orders_2015.sql
@@ -0,0 +1,65 @@
+/*
+
+IMPORT data/purchase_orders_2015.csv
+
+*/
+
+-- reference: http://stackoverflow.com/questions/17566573/convert-month-shortname-to-month-number
+-- mysql:
+
+-- create a "staging" table to temporarily house the data...
+
+DROP TABLE IF EXISTS temp_dc_purchase_orders;
+CREATE TABLE `temp_dc_purchase_orders` (
+  `Agency` VARCHAR(255) DEFAULT NULL,
+   `Commodity` VARCHAR(255) DEFAULT NULL,
+   `Supplier` VARCHAR(255) DEFAULT NULL,
+   `Ordered Date` VARCHAR(255) DEFAULT NULL,
+   `PO_#` VARCHAR(255) DEFAULT NULL,
+   `PO Amount` VARCHAR(255) DEFAULT NULL,
+   `OBJECTID` INT(11) DEFAULT NULL
+) ENGINE=INNODB DEFAULT CHARSET=utf8; -- this table definition was copied from sequel pro's "table info" menu after the .csv file was first imported...
+
+-- load the data into the staging table...
+
+LOAD DATA LOCAL INFILE '~/projects/gwu-business/washington-dc-open-data/data/purchase_orders_2015.csv'
+INTO TABLE temp_dc_purchase_orders
+FIELDS
+  TERMINATED BY ','
+  ENCLOSED BY '"'
+LINES TERMINATED BY '\n' -- mac-style line breaks
+  IGNORE 1 LINES
+;
+
+-- what are the physical storage requirements in terms of the `PO Amount` column? ...
+SELECT max(length(`PO Amount`)) FROM temp_dc_purchase_orders; -- 11 digits/characters
+
+-- transform the staging data into a more usable/friendly form ...
+DROP TABLE IF EXISTS dc_purchase_orders;
+CREATE TABLE dc_purchase_orders AS (
+  SELECT
+    po.objectid AS order_id
+    ,po.`po_#` AS order_number
+    ,po.agency AS agency_name
+    ,po.commodity AS commodity_name
+    ,po.supplier AS supplier_name
+    ,cast(
+        str_to_date(
+          concat(
+            concat("20",SUBSTRING_INDEX(po.`Ordered Date`, '-', -1)),
+            "-",
+            SUBSTRING_INDEX(SUBSTRING_INDEX(po.`Ordered Date`, '-', -2), '-', 1),
+            "-",
+            SUBSTRING_INDEX(po.`Ordered Date`, '-', 1)
+          ), '%Y-%b-%d') -- string formatted like a date
+          AS DATE -- string casted as "date" datatype
+    ) AS order_date
+    ,cast(po.`PO Amount` AS DECIMAL(12,2)) AS order_price_usd
+  FROM temp_dc_purchase_orders AS po
+);
+
+DROP TABLE IF EXISTS temp_dc_purchase_orders;
+
+SELECT * FROM dc_purchase_orders;
+
+--todo: indices ...
